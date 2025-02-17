eab8aad9ee58d6f07167066ef5480593b8638d4a
maraemanuella <maraemanuella132@gmail.com>
2025-02-17 00:41:04 -0300

    Update

--- t06-crud-php/src/controllers/users/login-user.php
+++ t06-crud-php/src/controllers/users/login-user.php
@@ -3,13 +3,14 @@ session_start();
 
 require_once __DIR__ . '/../../../database.php';
 require_once __DIR__ . "/../../dao/user-dao.php";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
 
     if (empty($email) || empty($password)) {
         $_SESSION['error'] = "Todos os campos são obrigatórios!";
-        header("Location: ../view/login.php");
+        header("Location: ../../../view/login.php");
         exit();
     }
 
@@ -24,12 +25,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $user = $users->getUserByEmail($email);
 
-    if ($user && password_verify($password, $user['password_hash'])) {
+    if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_name'] = $user['name'];
         $_SESSION['user_email'] = $user['email'];
         $_SESSION['LAST_ACTIVITY'] = time();
         $_SESSION['success'] = "Login realizado com sucesso!";
+        
         header("Location: ../../../view/profile.php");
         exit();
     } else {
