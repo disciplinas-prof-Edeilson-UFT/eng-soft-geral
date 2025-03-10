2d8f7efc1657534583f4032287bb1d88b4683fd5
lauroolim <lauroolimota743@gmail.com>
2025-03-10 14:14:38 -0300

    rename users.sql to db.sql

--- t05-session-php/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone VARCHAR(15) UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    bio TEXT
-);
